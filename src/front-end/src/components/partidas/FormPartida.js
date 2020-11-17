900d40fab1f1646e4c91ffde3a9fc546177052ef
llcruz1 <lucaslimacruz1@gmail.com>
2020-11-17 13:53:32 -0300

    Criando formulário 'Nova Partida'

--- src/front-end/src/components/partidas/FormPartida.js
+++ src/front-end/src/components/partidas/FormPartida.js
@@ -21,6 +21,11 @@ export default function FormPartida(props) {
                     <label>Data:</label> <input type="date" name="data" value={partida.data} onChange={handleInputChange} />
                     <br></br>
                     <br></br>
+                    <label>Árbitro:</label> <input type="text" name="arbitro_partida" maxlength="50" size="15" value={partida.arbitro} onChange={handleInputChange} />
+                    &nbsp;
+                    <label>Local:</label> <input type="text" name="local_partida" maxlength="50" size="15" value={partida.local} onChange={handleInputChange} />
+                    <br></br>
+                    <br></br>
                     <label>Time A:</label> <input type="text" name="time_A" maxlength="50" size="15" value={partida.time_A} onChange={handleInputChange} />
                     &nbsp;
                     <label>Gols</label> <input type="number" name="gols_time_A" min="0" max="99" value={partida.gols_time_A} onChange={handleInputChange} />
@@ -32,6 +37,16 @@ export default function FormPartida(props) {
                     <label>Time B:</label> <input type="text" name="time_B" maxlength="50" size="15" value={partida.time_B} onChange={handleInputChange} />
                     &nbsp;
                     <br/><br/>
+                    <hr></hr>
+                    <label>Jogador:</label> <input type="text" name="jogador_A" maxlength="50" size="15" value={partida.jogador_A} onChange={handleInputChange} />
+                    &nbsp;
+                    <label>Gols</label> <input type="number" name="gols_jogador_A" min="0" max="99" value={partida.gols_jogador_A} onChange={handleInputChange} />
+                    &nbsp;
+                    <label>Jogador:</label> <input type="text" name="jogador_B" maxlength="50" size="15" value={partida.jogador_B} onChange={handleInputChange} />
+                    &nbsp;
+                    <label>Gols</label> <input type="number" name="gols_jogador_B" min="0" max="99" value={partida.gols_jogador_B} onChange={handleInputChange} />
+                    <br></br>
+                    <br></br>
                     <input type="submit" value="Salvar" />
                 </form>
             </>
