268605d0d6407c8610ad8e90c7722f99be060c26
llcruz1 <lucaslimacruz1@gmail.com>
2020-11-19 12:14:41 -0300

    Criando botão 'Editar'

--- src/front-end/src/components/partidas/TabelaPartidas.js
+++ src/front-end/src/components/partidas/TabelaPartidas.js
@@ -1,31 +1,32 @@
+import React from 'react';
 import {Link} from "react-router-dom";
+import {useSelector, useDispatch} from 'react-redux';
 
 export default function ListagemPartida(props) {
   
-  function handleClickExcluirPartida(data){
-    props.setPartidas(props.partidas.filter((value) => value.data !== data));
+  const partidas = useSelector(state => state.partidas);
+  const dispatch = useDispatch();
+  
+  function handleClickExcluirPartida(id){
+      dispatch({type: 'delete_partida', payload:id})
+  }
+
+  function ProcurarPartida(data){
+    props.setPartidas(props.partidas.filter((value) => value.data === data));
   }
 
   return( <>
             <div id='lbl_titulo_pagina'>Listagem de Partidas</div><br/>
-            <Link to="/partidas/novo"><button id="Nova Partida" name="btn_nova_partida">Nova Partida</button></Link><br/><br/>
-            <br/><br/>
-            <TabelaPartidas partidas={props.partidas} onClickExcluirPartida={handleClickExcluirPartida}  />
+            <form>
+              <label>Data:</label> <input type="text" name="data" onChange={ProcurarPartida}/>
+              &nbsp;
+              <Link to="/partidas/novo"><button id="Nova Partida" name="btn_nova_partida">Nova Partida</button></Link><br/><br/>
+              <br/><br/>
+              <TabelaPartidas partidas={partidas} onClickExcluirPartida={handleClickExcluirPartida}/>
+            </form>
           </>
   );
 }
-
-const TabelaPartidas = (props) => {
-  return(
-          <table id="partidas" border="1">
-            <tbody>
-              {props.partidas.map((partida) =><LinhaPartida key={partida.data} partida={partida} 
-                                    onClickExcluirPartida={props.onClickExcluirPartida}/>)}
-            </tbody>
-          </table>
-  );
-}
-
 
 const LinhaPartida = (props) => {
   return(
@@ -36,7 +37,19 @@ const LinhaPartida = (props) => {
             <td>X</td>
             <td>{props.partida.gols_time_B}</td>
             <td>{props.partida.time_B}</td>
-            <td><button onClick={() => props.onClickExcluirPartida(props.partida.data)}>X</button></td>
+            <td><Link to={`/partidas/${props.partida.id}`}><button>Editar</button></Link></td>
+            <td><button id="excluir_partida" name="excluir_partida" onClick={() => props.onClickExcluirPartida(props.partida.id)}>Del</button></td>
           </tr>
+  );
+}
+
+function TabelaPartidas(props) {
+  return(
+          <table id="partidas" border="1">
+            <tbody>
+              {props.partidas.map((partida) =><LinhaPartida key={partida.id} partida={partida} 
+                                    onClickExcluirPartida={props.onClickExcluirPartida}/>)}
+            </tbody>
+          </table>
   );
 }
