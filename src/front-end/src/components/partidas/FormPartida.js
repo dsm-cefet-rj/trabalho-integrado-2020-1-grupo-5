663e29294284e044e51c5da3df07973977bacd30
llcruz1 <lucaslimacruz1@gmail.com>
2020-11-21 14:27:54 -0300

    Conditional Rendering 'Editar Partida/Nova Partida'

--- src/front-end/src/components/partidas/FormPartida.js
+++ src/front-end/src/components/partidas/FormPartida.js
@@ -6,7 +6,7 @@ import {addPartida, updatePartida} from './PartidasSlice'
 export default function FormPartida(props) {
     
     //inicializa o estado com o hook useState
-    const partidas = useSelector(state => state.partidas) 
+    const partidas = useSelector(state => state.partidas.partidas) 
     const history  = useHistory();
     const dispatch = useDispatch();
 
@@ -27,6 +27,15 @@ export default function FormPartida(props) {
         setPartida( {...partida, [e.target.name]: e.target.value});
     }
     
+    function handleAdicionaJogadorA(e) {
+        return(
+            <>
+                Teste
+            </>
+        );
+    }
+
+
     function handleSubmit(e){
         e.preventDefault();
         if(actionType === 'partidas/addPartida'){
@@ -38,37 +47,46 @@ export default function FormPartida(props) {
     }
 
     return( <>
-                <h1>Nova Partida</h1>
+                <h1>{(partida.id ?? 0) === 0 ? "Nova Partida" : "Editar Partida"}</h1>
+
                 <form onSubmit={handleSubmit}>
-                    <label>Data:</label> <input type="text" name="data" value={partida.data} onChange={handleInputChange} />
+                    <label>Data:</label><br/> 
+                    <input type="text" name="data" value={partida.data} onChange={handleInputChange} />
+                    <br></br><br></br>
+                    <label>Árbitro:</label><br/> 
+                    <input type="text" name="arbitro_partida" maxlength="50" size="15" value={partida.arbitro} onChange={handleInputChange} />
+                    <br></br><br></br>
+                    <label>Local:</label><br/> 
+                    <input type="text" name="local_partida" maxlength="50" size="15" value={partida.local} onChange={handleInputChange} />
                     <br></br>
                     <br></br>
-                    <label>Árbitro:</label> <input type="text" name="arbitro_partida" maxlength="50" size="15" value={partida.arbitro} onChange={handleInputChange} />
+                    <input type="text" name="time_A" maxlength="50" size="15" value={partida.time_A} onChange={handleInputChange} />
                     &nbsp;
-                    <label>Local:</label> <input type="text" name="local_partida" maxlength="50" size="15" value={partida.local} onChange={handleInputChange} />
-                    <br></br>
-                    <br></br>
-                    <label>Time A:</label> <input type="text" name="time_A" maxlength="50" size="15" value={partida.time_A} onChange={handleInputChange} />
-                    &nbsp;
-                    <label>Gols</label> <input type="number" name="gols_time_A" min="0" max="99" value={partida.gols_time_A} onChange={handleInputChange} />
+                    <input type="number" name="gols_time_A" min="0" max="99" value={partida.gols_time_A} onChange={handleInputChange} />
                     &nbsp;
                     X
                     &nbsp;
-                    <label>Gols</label> <input type="number" name="gols_time_B" min="0" max="99" value={partida.gols_time_B} onChange={handleInputChange} />
+                    <input type="number" name="gols_time_B" min="0" max="99" value={partida.gols_time_B} onChange={handleInputChange} />
                     &nbsp;
-                    <label>Time B:</label> <input type="text" name="time_B" maxlength="50" size="15" value={partida.time_B} onChange={handleInputChange} />
-                    &nbsp;
-                    <br/><br/>
+                    <input type="text" name="time_B" maxlength="50" size="15" value={partida.time_B} onChange={handleInputChange} />
                     <hr></hr>
-                    <label>Jogador:</label> <input type="text" name="jogador_A" maxlength="50" size="15" value={partida.jogador_A} onChange={handleInputChange} />
-                    &nbsp;
-                    <label>Gols</label> <input type="number" name="gols_jogador_A" min="0" max="99" value={partida.gols_jogador_A} onChange={handleInputChange} />
-                    &nbsp;
-                    <label>Jogador:</label> <input type="text" name="jogador_B" maxlength="50" size="15" value={partida.jogador_B} onChange={handleInputChange} />
-                    &nbsp;
-                    <label>Gols</label> <input type="number" name="gols_jogador_B" min="0" max="99" value={partida.gols_jogador_B} onChange={handleInputChange} />
+                    <div>
+                        <input classname="adiciona_jogador_A" type="button" name="adiciona_jogador_A" value="+A" onClick={handleAdicionaJogadorA}/>
+                        <input classname="adiciona_jogador_B" type="button" name="adiciona_jogador_B" value="+B" />
+                    </div>
                     <br></br>
-                    <br></br>
+    
+                    <label>Jogador Time A         Gols            </label><label>Jogador Time B         Gols</label><br/>
+    
+                    <input type="text" name="jogador_A" maxlength="50" size="15" value={partida.jogador_A} onChange={handleInputChange} />
+                    &nbsp;
+                    <input type="number" name="gols_jogador_A" min="0" max="99" value={partida.gols_jogador_A} onChange={handleInputChange} />
+                    &nbsp;
+
+                    <input type="text" name="jogador_B" maxlength="50" size="15" value={partida.jogador_B} onChange={handleInputChange} />
+                    &nbsp;
+                    <input type="number" name="gols_jogador_B" min="0" max="99" value={partida.gols_jogador} onChange={handleInputChange} />
+                    <br></br><br></br>
                     <input type="submit" value="Salvar" />
                 </form>
             </>
