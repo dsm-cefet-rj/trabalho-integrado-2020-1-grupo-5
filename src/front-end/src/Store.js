5869023e3660c6f870b241662c9913142359fda8
llcruz1 <lucaslimacruz1@gmail.com>
2020-11-22 17:01:05 -0300

    Adicionando os reducers de 'jogadores'

--- src/front-end/src/Store.js
+++ src/front-end/src/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import partidasReducer from './components/partidas/PartidasSlice'
+import jogadoresReducer from './components/jogadores/JogadoresSlice'
 
 export const store = configureStore({
     reducer: {
-        partidas: partidasReducer 
+        partidas: partidasReducer,
+        jogadores: jogadoresReducer
      /*aqui poderiam entrar mais reducers, um por chave do mapa */
     }
 })
