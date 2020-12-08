7b4ec4b46a98ae63372001e742c8af86e0772500
llcruz1 <lucaslimacruz1@gmail.com>
2020-12-08 15:59:27 -0300

    Melhorando layout da LinhaPartida e adicionando Dialog no botão Excluir

--- src/front-end/src/components/partidas/LinhaPartida.js
+++ src/front-end/src/components/partidas/LinhaPartida.js
@@ -10,7 +10,13 @@ import Hidden from '@material-ui/core/Hidden';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Azul from './camisaazul.png'
 import Vermelho from './camisavermelha.png'
-
+import Grid from '@material-ui/core/Grid';
+import {useHistory } from "react-router-dom";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const useStyles = makeStyles((theme) => ({
     margin: {
@@ -24,31 +30,66 @@ const useStyles = makeStyles((theme) => ({
 
 const LinhaPartida = (props) => {
     const classes = useStyles();
+    const history  = useHistory();
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+    
     if(props != null && props.partida != null && props.partida.id != null){
         return(
           <>
-            <ListItem>
-              <ListItem button component={Link} to = {`/partidas/visualizar/${props.partida.id}`}>
-                <Hidden xsDown>
-                  <ListItemText secondary= {new Date(props.partida.data).toLocaleDateString()}/>
-                </Hidden>
-                <ListItemAvatar>
-                <img className={classes.image} alt={props.partida.time_A} src={Azul}/>
-                  <ListItemText secondary={props.partida.time_A}></ListItemText>
-                </ListItemAvatar>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <ListItemText  primary={props.partida.gols_time_A}/>
-                <ListItemText  primary="X"/>
-                <ListItemText  primary={props.partida.gols_time_B}/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <ListItemAvatar>
-                  <img className={classes.image} alt={props.partida.time_B} src={Vermelho}/>
-                  <ListItemText secondary={props.partida.time_B}></ListItemText>
-                </ListItemAvatar>
-              </ListItem>           
+            <ListItem button component={Link} to = {`/partidas/visualizar/${props.partida.id}`}>
+              <Grid className={classes.root} container direction="row">           
+                <Grid direction="column" item xs={12} container alignItems="center">
+                      <ListItemText secondary={new Date(props.partida.data).toLocaleDateString()}/>            
+                </Grid>
+                <Grid direction="column" item xs={4} container alignItems="center" >
+                    <img className={classes.image} alt={props.partida.time_A} src={Azul}/>
+                    <br/>
+                    <ListItemText secondary={props.partida.time_A}/>
+                </Grid>
+                <Grid direction="column" item xs={4} container alignItems="center">
+                    <h3>{props.partida.gols_time_A} X {props.partida.gols_time_B}</h3>
+                </Grid>
+                <Grid direction="column" item xs={4} container alignItems="center">
+                    <img className={classes.image} alt={props.partida.time_B} src={Vermelho}/>
+                    <br/>
+                    <ListItemText secondary={props.partida.time_B}/>
+                </Grid>
+              </Grid>        
                 <Link to={`/partidas/${props.partida.id}`}><IconButton id="edita_partida" Link to={`/partidas/${props.partida.id}`} ><EditIcon/></IconButton></Link>
-                <IconButton id="deleta_partida" name="excluir_partida" onClick={() => props.onClickExcluirPartida(props.partida.id)}><DeleteIcon /></IconButton>
+                <IconButton id="deleta_partida" name="excluir_partida" onClick={handleClickOpen}><DeleteIcon /></IconButton>
             </ListItem>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                >
+                  <DialogTitle id="alert-dialog-title">{"Deseja mesmo excluir o item?"}</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                      Você está prestes a excluir o item selecionado.
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <IconButton onClick={handleClose}>
+                      Cancelar
+                    </IconButton>
+                    <IconButton onClick={() => {props.onClickExcluirPartida(props.partida.id)}} color="secondary" autoFocus>
+                      Excluir
+                    </IconButton>
+                  </DialogActions>
+                </Dialog>
+            <br/>
           </>
        );
     }else{
